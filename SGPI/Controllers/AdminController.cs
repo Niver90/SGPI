fa21d7fafaa14911c008efb71160aea1ba117c56
ESTUDIANTE <ESTUDIANTE@DESKTOP-PL0F26T>
2022-04-06 11:12:03 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGPI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdministrarUsuario()
+        {
+            return View();
+        }public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+    }
+}
